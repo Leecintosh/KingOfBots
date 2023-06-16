cba795a51010e2602efda4d3e2c3c08c1e59ff27
Xu Li <lx993204681@gmail.com>
2023-06-16 09:37:05 -0700

    finish creating navigation bar and basic game map generation

---
+++ web/src/components/NavBar.vue
@@ -0,0 +1,40 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <RouterLink class="navbar-brand" to="{name: 'home'}">King Of Bots</RouterLink>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <router-link class="nav-link" active-class="active" :to="{ name: 'pk_index' }">Battle</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" active-class="active" :to="{ name: 'record_index' }">Rooms</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" active-class="active" :to="{ name: 'ranklist_index' }">Rank</router-link>
+                    </li>
+                </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Leex
+                        </a>
+                        <ul class="dropdown-menu">
+                            <router-link class="dropdown-item" :to="{ name: 'user_bot_index' }">My Profile</router-link>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Sign Out</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    
+</style>
