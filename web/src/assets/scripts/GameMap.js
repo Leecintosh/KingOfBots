db08aaa87c54fe60df6af79fef159649fcce55de
Xu Li <lx993204681@gmail.com>
2023-06-19 00:38:28 -0700

    finish drawing snakes, setting their logistics and dealing with keyboard inputs

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -1,5 +1,6 @@
 import { AcGameObject } from "./AcGameObject";
 import { Wall } from "./Wall";
+import { Snake } from "./Snake";
 
 export class GameMap extends AcGameObject {
     constructor(ctx, parent) {
@@ -10,13 +11,18 @@ export class GameMap extends AcGameObject {
         this.L = 0;
 
         this.rows = 13;
-        this.cols = 13;
+        this.cols = 14;
 
         this.inner_walls_count = 20;
         this.walls = [];
+
+        this.snakes = [
+            new Snake({id: 0, color: "#4876EC", r: this.rows - 2, c: 1}, this),
+            new Snake({id: 1, color: "#F94848", r: 1, c: this.cols - 2}, this),
+        ];
     }
 
-
+    // using flood fill algorithm to check connectivity
     check_connectivity(g, sx, sy, tx, ty) {
         if (sx == tx && sy == ty) return true;
         g[sx][sy] = true;
@@ -55,10 +61,10 @@ export class GameMap extends AcGameObject {
                 let c = parseInt(Math.random() * this.cols);
 
                 // avoid repeated walls and walls near the start point
-                if (g[r][c] || g[c][r]) continue;
+                if (g[r][c] || g[this.rows - 1 - r][this.cols - 1 - c]) continue;
                 if (r == this.rows - 2 && c == 1 || r == 1 && c == this.cols - 2) continue;
 
-                g[r][c] = g[c][r] = true;
+                g[r][c] = g[this.rows - 1 - r][this.cols - 1 - c] = true;
                 break;
             }
         }
@@ -76,10 +82,28 @@ export class GameMap extends AcGameObject {
         return true;
     }
 
+    add_listening_events() {
+        this.ctx.canvas.focus();
+
+        const [snake0, snake1] = this.snakes;
+        this.ctx.canvas.addEventListener("keydown", e => {
+            if (e.key === 'w') snake0.set_direction(0);
+            else if (e.key === 'd') snake0.set_direction(1);
+            else if (e.key === 's') snake0.set_direction(2);
+            else if (e.key === 'a') snake0.set_direction(3);
+            else if (e.key === 'ArrowUp') snake1.set_direction(0);
+            else if (e.key === 'ArrowRight') snake1.set_direction(1);
+            else if (e.key === 'ArrowDown') snake1.set_direction(2);
+            else if (e.key === 'ArrowLeft') snake1.set_direction(3);
+        });
+    }
+
     start() {
         for (let i = 0; i < 1000; i++) {
             if (this.create_walls()) break;
         }
+
+        this.add_listening_events();
     }
 
     update_size() {
@@ -88,18 +112,55 @@ export class GameMap extends AcGameObject {
         this.ctx.canvas.height = this.L * this.rows;
     }
 
+    check_ready() { // check if all snakes are ready
+        for (const snake of this.snakes) {
+            if (snake.status !== "idle") return false;
+            if (snake.direction === -1) return false;
+        }
+        return true;
+    }
+
+    next_step() {
+        for (const snake of this.snakes) {
+            snake.next_step();
+        }
+    }
+
+    check_valid(cell) { // check if position is valid: no collision with walls and snakes body
+        for (const wall of this.walls) {
+            if (wall.r === cell.r && wall.c === cell.c) return false;
+        }
+
+        for (const snake of this.snakes) {
+            let k = snake.cells.length;
+            if (!snake.check_tail_increasing()) k --; // if tail is not increasing, no need to check the last cell
+            for (let i = 0; i < k; i++) {
+                if (snake.cells[i].r === cell.r && snake.cells[i].c === cell.c) return false;
+            }
+        }
+        return true;
+    }
+
     update() {
         this.update_size();
+        if (this.check_ready()) {
+            this.next_step();
+        }
         this.render();
     }
 
     render() {
         const color_even = "#AAD751", color_odd = "#A2D149";
-        for (let i = 0; i < this.rows; i++) {
-            for (let j = 0; j < this.cols; ++j) {
-                this.ctx.fillStyle = (i + j) % 2 == 0 ? color_even : color_odd;
-                this.ctx.fillRect(j * this.L, i * this.L, this.L, this.L);
+        for (let r = 0; r < this.rows; r ++ ) {
+            for (let c = 0; c < this.cols; c ++ ) {
+                if ((r + c) % 2 == 0) {
+                    this.ctx.fillStyle = color_even;
+                } else {
+                    this.ctx.fillStyle = color_odd;
+                }
+                this.ctx.fillRect(c * this.L, r * this.L, this.L, this.L);
             }
         }
     }
+
 }
---
+++ web/src/assets/scripts/Snake.js
@@ -0,0 +1,146 @@
+import { AcGameObject } from "./AcGameObject";
+import { Cell } from "./Cell";
+
+export class Snake extends AcGameObject {
+    constructor(info, gamemap) {
+        super();
+
+        this.id = info.id;
+        this.color = info.color;
+        this.gamemap = gamemap;
+
+        this.cells = [new Cell(info.r, info.c)]; // store the cells of the snake, cells[0] is the head
+        this.next_cell = null; // the next cell to move to
+
+        this.speed = 5; // cells per second
+        this.direction = -1; // -1: stop, 0: up, 1: right, 2: down, 3: left
+        this.status = "idle"; // idle, move, die
+
+        this.dr = [-1, 0, 1, 0]; // row offset for each direction 
+        this.dc = [0, 1, 0, -1]; // col offset for each direction
+
+        this.step = 0; // current step
+        this.eps = 1e-2; // epsilon for comparing float numbers
+
+        this.eye_direction = 0; // 0: right, 1: left, 2: up, 3: down
+        if (this.id === 1) this.eye_direction = 2; // left bottom snake looks up, right top snake looks down
+
+        this.eye_dx = [ // eye x offset for each direction
+            [-1, 1], // right
+            [1, 1], // left
+            [1, -1], // up
+            [-1, -1] // down
+        ];
+
+        this.eye_dy = [ // eye y offset for each direction
+            [-1, -1], // right
+            [-1, 1], // left
+            [1, 1], // up
+            [-1, 1] // down
+        ];
+    }
+
+    start() {
+    }
+
+    set_direction(d) {
+        this.direction = d;
+    }
+
+    check_tail_increasing() { // check if the tail is increasing at current step
+        if (this.step <= 10) return true;
+        if (this.step % 3 === 1) return true;
+        return false;
+    }
+
+    next_step() { // go to the next step
+        const d = this.direction;
+        this.next_cell = new Cell(this.cells[0].r + this.dr[d], this.cells[0].c + this.dc[d]);
+        this.eye_direction = d;
+        this.direction = -1; // reset direction
+        this.status = "move";
+        this.step ++;
+
+        const k = this.cells.length;
+        for (let i = k; i > 0; i--) {
+            this.cells[i] = JSON.parse(JSON.stringify(this.cells[i - 1]));
+        }
+
+        if (!this.gamemap.check_valid(this.next_cell)) { // if the next cell is not valid(collision), die
+            this.status = "die";
+        }
+    }
+
+    update_move() {
+        const dx = this.next_cell.x - this.cells[0].x;
+        const dy = this.next_cell.y - this.cells[0].y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.eps) { // reach the target cell
+            this.cells[0] = this.next_cell; // add a new cell as the head
+            this.next_cell = null;
+            this.status = "idle"; // stop moving
+
+            if (!this.check_tail_increasing()) { // if the tail is not increasing, remove the tail
+                this.cells.pop(); // remove the tail
+            }
+        } else {
+            const move_distance = this.speed * this.timedelta / 1000; // distance between 2 frames
+            this.cells[0].x += dx / distance * move_distance;
+            this.cells[0].y += dy / distance * move_distance;
+
+            if (!this.check_tail_increasing()) { // add a new cell as the tail
+                const k = this.cells.length;
+                const tail = this.cells[k - 1], tail_target = this.cells[k - 2];
+                const tail_dx = tail_target.x - tail.x;
+                const tail_dy = tail_target.y - tail.y;
+                tail.x += tail_dx / distance * move_distance;
+                tail.y += tail_dy / distance * move_distance;
+
+            }
+        }
+    }
+
+    update() { // every frame update, 60 fps
+        if (this.status === "move") {
+            this.update_move();
+        }
+        this.render();
+    }
+
+    render() {
+        const L = this.gamemap.L;
+        const ctx = this.gamemap.ctx;
+
+        ctx.fillStyle = this.color;
+        if (this.status === "die") {
+            ctx.fillStyle = "white";
+        }
+
+        for (const cell of this.cells) {
+            ctx.beginPath();
+            ctx.arc(cell.x * L, cell.y * L, L / 2 * 0.8, 0, 2 * Math.PI);
+            ctx.fill();
+        }
+
+        for (let i = 1; i < this.cells.length; i++) {
+            const a = this.cells[i - 1], b = this.cells[i];
+            if (Math.abs(a.x - b.x) < this.eps && Math.abs(a.y - b.y) < this.eps) continue;
+            if (Math.abs(a.x - b.x) < this.eps) {
+                ctx.fillRect((a.x - 0.4) * L, Math.min(a.y, b.y) * L, L * 0.8, Math.abs(a.y - b.y) * L);
+            } else {
+                ctx.fillRect(Math.min(a.x, b.x) * L, (a.y - 0.4) * L, Math.abs(a.x - b.x) * L, L * 0.8);
+            }
+        }
+
+        ctx.fillStyle = "black";
+        for (let i = 0; i < 2; i++) {
+            const eye_x = (this.cells[0].x + this.eye_dx[this.eye_direction][i] * 0.15) * L;
+            const eye_y = (this.cells[0].y + this.eye_dy[this.eye_direction][i] * 0.15) * L;
+
+            ctx.beginPath();
+            ctx.arc(eye_x, eye_y, L * 0.05, 0, Math.PI * 2);
+            ctx.fill();
+        }
+    }
+}
